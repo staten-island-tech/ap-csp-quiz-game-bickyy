f4f6fff70717f876460b0df28b69861e4111f830
bickyy <vicky.zhou21@sitechhs.com>
2020-10-09 18:44:19 -0400

    .

--- src/src/js/Dom.js
+++
@@ -1,7 +0,0 @@
-const DOMSelectors = {  
-    quiz: document.querySelector(".quiz"),
-    results: document.querySelector(".results"),
-    submitButton: document.querySelector(".submit-btn"),
-  };
-
-export { DOMSelectors };
